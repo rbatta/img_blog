064b6e9b32cf6eeb04da94dcb2ace1218bc3b345
Risa Batta <rbatta@gmail.com>
2013-09-04 13:15:14 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ImgBlog::Application.config.secret_key_base = '9b6f5dcfed9a9c14f27a4deefcf77c4e382f2b3e6536c8417fe4693cd229fe82f683958e7d897afdfd0108874bf894f433bab887498c8f4b9780c9796fee5281'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ImgBlog::Application.config.session_store :cookie_store, key: '_img_blog_session'
