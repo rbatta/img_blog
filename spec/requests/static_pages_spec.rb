513cc3584ed2f200579a9cc5452807eb55a07896
Risa Batta <rbatta@gmail.com>
2013-09-08 13:02:16 -0400

    added user controller and spec files, added signup base page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,14 +7,14 @@ describe "StaticPages" do
   describe "Home page" do
     before { visit root_path }
     it { should have_content('Image Blog') }
-    it { should have_title('Image Blog') }
+    it { should have_title(full_title('')) }
     it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
     before { visit help_path }
   	it { should have_content('Help') }
-    it { should have_title('Help') }
+    it { should have_title(full_title('Help')) }
   end
 
   describe "About me" do
