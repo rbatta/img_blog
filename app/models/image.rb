ea077c5235aa6379b773388db59a6628fb64be1b
Risa Batta <rbatta@gmail.com>
2013-10-01 15:01:34 -0400

    added attr_protected to image model

--- app/models/image.rb
+++ app/models/image.rb
@@ -4,5 +4,6 @@ class Image < ActiveRecord::Base
 	validates :user_id, presence: true
 	validates :description, presence: true, length: { maximum: 250 }
 	validates :img_name, presence: true, length: { maximum: 80 }
+	attr_protected :user_id
 
 end
